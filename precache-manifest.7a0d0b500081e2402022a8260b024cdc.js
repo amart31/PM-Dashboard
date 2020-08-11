4c4a912013128e2c884736d96212bb7770a9958a
Andy Martinez <martinez1751@gmail.com>
2020-08-11 18:16:13 -0400

    Updates

---
+++ precache-manifest.7a0d0b500081e2402022a8260b024cdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91a53ef36447397b9b1764f6d6acc2c",
+    "url": "/PM-Dashboard/index.html"
+  },
+  {
+    "revision": "5ac08f06dda0364c5d9c",
+    "url": "/PM-Dashboard/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "e7b68e910fdb8b32c003",
+    "url": "/PM-Dashboard/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "5ac08f06dda0364c5d9c",
+    "url": "/PM-Dashboard/static/js/2.33fa9407.chunk.js"
+  },
+  {
+    "revision": "286624c8d95769e3759a2a7869634d07",
+    "url": "/PM-Dashboard/static/js/2.33fa9407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b68e910fdb8b32c003",
+    "url": "/PM-Dashboard/static/js/main.d0d6b416.chunk.js"
+  },
+  {
+    "revision": "8b57f32bd749cff5ef64",
+    "url": "/PM-Dashboard/static/js/runtime-main.4d4bb08c.js"
+  }
+]);
